fa23d34bd792b9696c82d00579f5c5d37de59f25
tristan-orourke <tristan.m.orourke@gmail.com>
2022-07-03 13:05:40 -0400

    Add background, music, and fix bug

--- Main.gd
+++ Main.gd
@@ -5,13 +5,14 @@ var score
 
 func _ready():
 	randomize()
-	new_game()
 
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 	
 func new_game():
 	score = 0
@@ -20,6 +21,7 @@ func new_game():
 	$HUD.show_message("Get Ready")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$Music.play()
 	
 	
 func _on_StartTimer_timeout():
