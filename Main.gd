abb4213b15c5bc3d0a90dbe6c6d08af3e4fcafbc
tristan-orourke <tristan.m.orourke@gmail.com>
2022-07-03 12:50:16 -0400

    Remove old mob group at game start

--- Main.gd
+++ Main.gd
@@ -15,6 +15,7 @@ func game_over():
 	
 func new_game():
 	score = 0
+	get_tree().call_group("mobs", "queue_free")
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	$Player.start($StartPosition.position)
