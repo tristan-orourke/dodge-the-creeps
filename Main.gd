2d6664b1be1442b84344ffd2dadbbc5e43f9aca1
tristan-orourke <tristan.m.orourke@gmail.com>
2022-07-03 12:48:42 -0400

    Add the HUD

--- Main.gd
+++ Main.gd
@@ -11,9 +11,12 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
 	score = 0
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 	
@@ -24,6 +27,7 @@ func _on_StartTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_MobTimer_timeout():
 	# Choose a random location on Path2D.
